b70a94915dea7ffff54854c0ab266fb7c8beee6a
HelloWorldSungin <38519039+HelloWorldSungin@users.noreply.github.com>
2018-05-07 14:23:46 -0700

    Add files via upload

--- ops.v
+++ ops.v
@@ -25,15 +25,9 @@ endmodule
 module mux4 #(parameter WORD_SIZE_BIT = 32) (
 	input  [1:0] s,
 	input  [WORD_SIZE_BIT-1:0] d0, d1, d2, d3,
-	output reg [WORD_SIZE_BIT-1:0] y
+	output [WORD_SIZE_BIT-1:0] y
 );
-always @ (*)
-  begin
-    case(s)
-      2'b00 : y <= d0;
-      2'b01 : y <= d1;
-      2'b10 : y <= d2;
-      2'b11 : y <= d3;
-    endcase
-  end
+
+assign y = (s == 2'd0) ? d0 : ((s == 2'd1) ? d1 : ((s == 2'd2) ? d2 : d3));
+
 endmodule
